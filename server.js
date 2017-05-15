b5d546b4e3dc4fd967f3587bf2999281e4917bff
Mathieu Girard <mat.girard@gmail.com>
2017-05-15 13:36:31 +0000

    initial

---
+++ server.js
@@ -0,0 +1,88 @@
+const IOLINC_ID = "22.D4.EC"
+const PLM_PORT = "/dev/ttyUSB1"
+const API_PORT = 3000
+
+var Insteon = require('home-controller').Insteon;
+var express = require('express');
+
+var app = express();
+
+
+var plm = new Insteon();
+plm.serial(PLM_PORT, function () {
+  app.listen(API_PORT);
+});
+
+
+app.get('/light/:id/on', function(req, res){
+  plm.light(req.params.id).turnOn()
+  .then(function (status) {
+    if(status.response) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+app.get('/light/:id/off', function(req, res){
+  plm.light(req.params.id).turnOff()
+  .then(function (status) {
+    if(status.response) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+
+app.get('/light/:id/dim/:level', function(req, res){
+  plm.light(req.params.id).turnOn(req.params.level)
+  .then(function (status) {
+    if(status.response) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+
+app.get('/light/:id/status', function(req, res){
+  plm.light(req.params.id).level()
+  .then(function (level) {
+    res.send(level.toString());
+  });
+});
+
+
+app.get('/garage/button', function(req, res){
+  var io = plm.ioLinc(IOLINC_ID); 
+  io.relayOn()
+  .then(function (status) {
+    if(status.response) {
+      io.relayOff()
+      .then(function (status) {
+        if(status.response) {
+          res.sendStatus(200);
+        } else {
+          res.sendStatus(404);
+        }
+      });
+      return;
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+
+app.get('/garage/status', function(req, res){
+  plm.ioLinc(IOLINC_ID).status()
+  .then(function (status) {
+    res.send(((status.sensor == 'off') ? 'ouvert' : 'fermé'));
+  });
+});
+
+
